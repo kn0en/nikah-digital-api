01bd99f033b8ab47eb39c930fae3f3143dd65ba0
kn0en <pratamaoktavian66@gmail.com>
2024-02-05 01:07:07 +0700

    coba commit

--- resources/views/welcome.kita.php
+++ resources/views/welcome.kita.php
@@ -9,7 +9,7 @@
     <meta name="theme-color" content="#ffffff">
     <meta name="color-scheme" content="light">
     <title>Kamu - PHP Framework</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://nikah-digital-api.vercel.app//css/app.css">
     <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('kamu.png') }}">
 </head>
 
@@ -24,7 +24,7 @@
         </div>
     </div>
 
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="https://nikah-digital-api.vercel.app/js/app.js"></script>
 </body>
 
 </html>
